1c693aea77b6a6cca0ee29e157b3a302ca5d0b93
Biplab Naskar <42195840+Biplab-123@users.noreply.github.com>
2020-12-28 10:58:40 +0530

    Add files via upload

---
+++ automation/amazon search.py
@@ -0,0 +1,5 @@
+##basically search on google open amzon/flipkart to watch out deals
+from selenium import webdriver 
+from time import sleep
+driver = webdriver.Chrome("C:/Users/dell/Downloads/chromedriver.exe") 
+driver.get('https://www.google.com')
