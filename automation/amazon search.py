773529b50dff747d5bd5a008578da3ad4e35d2ab
Biplab Naskar <42195840+Biplab-123@users.noreply.github.com>
2021-01-02 16:01:30 +0530

    Delete amazon search.py

--- automation/amazon search.py
+++
@@ -1,5 +0,0 @@
-##basically search on google open amzon/flipkart to watch out deals
-from selenium import webdriver 
-from time import sleep
-driver = webdriver.Chrome("C:/Users/dell/Downloads/chromedriver.exe") 
-driver.get('https://www.google.com')
