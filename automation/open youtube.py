2199372951c0eae7a0e25b3af74075153f0edfe2
Biplab Naskar <42195840+Biplab-123@users.noreply.github.com>
2021-01-02 16:02:08 +0530

    Delete open youtube.py

--- automation/open youtube.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver 
-from time import sleep
-driver = webdriver.Chrome("C:/Users/dell/Downloads/chromedriver.exe") 
-driver.get('https://www.youtube.com')
-searchbox=driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/ytd-searchbox/form/div/div[1]/input')
-searchbox.send_keys('mortal')
-searchbutton=driver.find_element_by_xpath('/html/body/ytd-app/div/div/ytd-masthead/div[3]/ytd-searchbox/form/button')
-searchbutton.click()
-sleep(10)
